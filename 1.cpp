89ac3ccc4adc0484a3c379c272193903e2de67f8
krishna <mandalkrishna739@gmail.com>
2025-04-01 21:43:22 +0545

    first commit

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+
+    cout << "\n=============================================================" << endl;
+    cout << "||\t\t      !!( Welcome )!!                      ||" << endl;
+    cout << "||\t\t Pharmacy Management System                ||" << endl;
+    cout << "||\t\t Biratnagar, Morang ( NEPAL )              ||" << endl;
+    cout << "=============================================================" << endl;
+    cout << "   1> Add Medicine" << endl;
+    cout << "   1> Update Medicine" << endl;
+    cout << "   2> Search Medicine" << endl;
+    cout << "   5> Delete Medicine" << endl;
+    cout << "   3> View Medicine List" << endl;
+    cout << "   5> Medicine" << endl;
+    cout << "   3> Exit program" << endl;
+    cout << "_____________________________________________________________" <<endl;
+    cout << "  Enter your choice = "<<endl;
+
+    return 0;
+}
